5a9ff7f2b133c926576ec378178c86690fb2cdb7
6suk <koandjo9116@gmail.com>
2024-03-30 12:31:21 +0900

    [✨feat] Server : 방 삭제 delete api 생성 및 에러 핸들링

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -1,5 +1,11 @@
 import uuid from 'uuid4';
-import { updateRoom } from '../service/room.service.js';
+import {
+  getOtherRooms,
+  getRoomById,
+  removeRoom,
+  updateRoom,
+} from '../service/room.service.js';
+import { jsonRemove } from '../utils/jsonFileManager.js';
 
 /**
  *  [
@@ -25,17 +31,45 @@ export const createdRoom = async (req, res) => {
     const newRoom = {
       id: uuid(),
       title,
-      created_user: currentUser.name,
+      created_user_id: currentUser.id,
       created_at: timestamp,
       updated_at: timestamp,
       messages: [],
     };
 
-    // ser json data
+    // set json data
     await updateRoom(newRoom);
     res.status(200).json(newRoom);
   } catch (error) {
     console.log('🚨 CreatedRoom Controller Error! : ', error);
+    res.status(500).json({
+      error: 'Server Error!',
+    });
+  }
+};
+
+export const removedRoom = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // 방이 존재 하는지
+    const room = await getRoomById(id);
+    if (!room)
+      return res.status(401).json({ error: '존재하지 않는 방입니다.' });
+
+    // created_user와 요청한 user가 같은지
+    if (req.user.id !== room.created_user_id)
+      return res.status(403).json({ error: '해당 방의 삭제 권한이 없습니다!' });
+
+    const otherRooms = await getOtherRooms(id);
+    // set json data
+    await removeRoom(otherRooms);
+    res.status(200).json({
+      room,
+      message: '방이 삭제 되었습니다!',
+    });
+  } catch (error) {
+    console.log('🚨 RemovedRoom Controller Error! : ', error);
     res.status(500).json({
       error: 'Server Error!',
     });
--- server/service/room.service.js
+++ server/service/room.service.js
@@ -1,6 +1,13 @@
-import { jsonRemove, jsonUpdate } from '../utils/jsonFileManager.js';
+import {
+  jsonGetOneById,
+  jsonGetOthersById,
+  jsonRemove,
+  jsonUpdate,
+} from '../utils/jsonFileManager.js';
 
 const fileName = 'room.json';
 
 export const updateRoom = async newData => jsonUpdate({ fileName, newData });
 export const removeRoom = async newData => jsonRemove({ fileName, newData });
+export const getOtherRooms = async id => jsonGetOthersById({ fileName, id });
+export const getRoomById = async id => jsonGetOneById({ fileName, id });
