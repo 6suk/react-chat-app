b1cbdb99e26dd9c1a2e78da39276d24a1330ecb5
6suk <koandjo9116@gmail.com>
2024-04-02 20:44:43 +0900

    [💄style] daisyUi 설치 및 세팅

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#cddeff',
+          'primary-content': '#5a81fa ',
+          secondary: '#2C3D8F',
+          'secondary-content': '#f2f5ff',
+          accent: '#00ffff',
+          neutral: '#ff00ff',
+          'base-100': '#ffffff',
+          'base-200': '#f8f9fd',
+          'base-300': '#f2f5ff',
+          'base-content': '#1f1f1f',
+          info: '#0000ff',
+          success: '#00ff00',
+          warning: '#00ff00',
+          error: '#ff0000',
+        },
+      },
+      'light',
+      'dark',
+    ],
+  },
+  plugins: [daisyui],
 };
