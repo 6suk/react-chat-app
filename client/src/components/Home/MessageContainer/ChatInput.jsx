4dd594ab0c2f8b227d20c4503324aea4fe6d5cd0
6suk <koandjo9116@gmail.com>
2024-04-04 23:02:04 +0900

    [✨feat] SendMessage Fetch 구현

---
+++ client/src/components/Home/MessageContainer/ChatInput.jsx
@@ -0,0 +1,39 @@
+import { IoIosSend } from 'react-icons/io';
+import useSendMessage from '../../../hooks/useSendMessage';
+import { useState } from 'react';
+
+const ChatInput = ({ disabled = false }) => {
+  const { isLoading, sendMessage } = useSendMessage();
+  const [message, setMessage] = useState('');
+
+  return (
+    <form
+      className="relative w-full"
+      onSubmit={e => {
+        e.preventDefault();
+        sendMessage(message);
+        setMessage('');
+      }}
+    >
+      <input
+        name="message"
+        type="text"
+        placeholder="Type here"
+        className="input w-full bg-secondary-content focus:outline-none disabled:bg-gray-200"
+        disabled={disabled}
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button
+        className="absolute inset-y-0 end-0 mx-4 my-2 flex items-center text-secondary disabled:hidden"
+        type="submit"
+        disabled={disabled || isLoading}
+      >
+        <IoIosSend size={30} />
+        {/* <span className="loading loading-spinner bg-secondary"></span> */}
+      </button>
+    </form>
+  );
+};
+
+export default ChatInput;
