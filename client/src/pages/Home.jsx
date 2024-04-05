fe0ecc83dbf4304c0cef5b4a4262a314de8fd1b5
6suk <koandjo9116@gmail.com>
2024-04-05 16:37:25 +0900

    [💄style] 반응형 스타일 수정

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import SideBar from '../components/Home/SideBar/SideBar';
 
 const Home = () => {
   return (
-    <div className="flex h-full w-full flex-col overflow-hidden rounded-lg bg-white bg-opacity-10 bg-clip-padding backdrop-blur-lg backdrop-filter sm:h-5/6 sm:w-10/12 sm:flex-row">
+    <div className="flex h-full w-full flex-col overflow-hidden rounded-lg bg-white bg-opacity-10 bg-clip-padding backdrop-blur-lg backdrop-filter md:h-5/6 md:w-10/12 md:flex-row">
       <SideBar />
       <MessageContainer />
     </div>
