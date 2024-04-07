222c58f615f3eb9884302786ef1cb2d4c7326a78
6suk <koandjo9116@gmail.com>
2024-04-07 23:05:13 +0900

    [⛏setting] tailwind config 파일 수정
    + 추후 삭제될 임시 코드

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import protectRoute from '../middleware/protect.route.js';
-import { otherUsersList } from '../controllers/user.controller.js';
+import { getUser, otherUsersList } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', protectRoute, otherUsersList);
+router.get('/:id', getUser); // 임시
 
 export default router;
